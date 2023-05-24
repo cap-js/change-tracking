fdf7274fbe52f8b05256b39c0226babf9634d331
Daniel Hutzel <daniel.hutzel@sap.com>
2023-05-24 18:58:00 +0200

    mini refactoring

--- cds-plugin.js
+++ cds-plugin.js
@@ -1,33 +1,40 @@
-const cds = require ('@sap/cds/lib')
+const cds = require('@sap/cds/lib')
+
+const isChangeTracked = (entity) => (
+  entity['@changelog']
+  || entity['@changelog.keys']
+  // || entity.elements && Object.values(entity.elements).some(e => e['@changelog'])
+)
+
 
 // Unfold @changelog annotations in loaded model
 cds.on('loaded', m => {
 
   // Get definitions from Dummy entity in our models
   const { 'sap.changelog.aspect': aspect } = m.definitions; if (!aspect) return // some other model
-  const { '@UI.Facets': [facet], elements: {changes} } = aspect
+  const { '@UI.Facets': [facet], elements: { changes } } = aspect
   changes.on.pop() // remove ID -> filled in below
 
   for (let name in m.definitions) {
     const entity = m.definitions[name]
-    if (entity['@changelog'] || entity['@changelog.keys']) {
+    if (isChangeTracked(entity)) {
 
       // Determine entity keys
-      const keys = [], {elements:elms} = entity
+      const keys = [], { elements: elms } = entity
       for (let e in elms) if (elms[e].key) keys.push(e)
 
       // Add association to ChangeView...
-      const on = [...changes.on]; keys.forEach((k,i) => { i && on.push('||'); on.push({ref:[k]}) })
+      const on = [...changes.on]; keys.forEach((k, i) => { i && on.push('||'); on.push({ ref: [k] }) })
       const assoc = { ...changes, on }
       const query = entity.projection || entity.query?.SELECT
       if (query) {
-        (query.columns ??= ['*']).push({ as: 'changes', cast: assoc})
+        (query.columns ??= ['*']).push({ as: 'changes', cast: assoc })
       } else {
         entity.elements.changes = assoc
       }
 
       // Add defaults for @changelog.keys
-      entity['@changelog.keys'] ??= keys.map(k => ({'=':k}))
+      entity['@changelog.keys'] ??= keys.map(k => ({ '=': k }))
 
       // Add UI.Facet for Change History List
       entity['@UI.Facets']?.push(facet)
@@ -36,4 +43,22 @@ cds.on('loaded', m => {
 })
 
 // Add generic change tracking handlers
-cds.on('served', ()=> require("./lib/change-log")())
+cds.on('served', () => {
+  const { _logChanges, _afterReadChangeView } = require("./lib/change-log")
+  for (const srv of cds.services) {
+    if (srv instanceof cds.ApplicationService) {
+      let any = false
+      for (const entity of Object.values(srv.entities)) {
+        if (isChangeTracked(entity) || Object.values(entity.elements).some(e => e['@changelog'])) {
+          cds.db.before("CREATE", entity, _logChanges)
+          cds.db.before("UPDATE", entity, _logChanges)
+          cds.db.before("DELETE", entity, _logChanges)
+          any = true
+        }
+      }
+      if (any && srv.entities.ChangeView) {
+        srv.after("READ", srv.entities.ChangeView, _afterReadChangeView)
+      }
+    }
+  }
+})
