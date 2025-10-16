a133a50a64507d0d275dcf1407caa27e8fcbbc17
Marten Schiwek <marten.schiwek@sap.com>
2025-10-16 14:46:57 +0200

    Added test and feature flag

--- tests/integration/incidents-app.test.js
+++ tests/integration/incidents-app.test.js
@@ -1,36 +1,57 @@
 const cds = require("@sap/cds")
 const path = require("path")
 const app = path.join(__dirname, "../incidents-app")
-const { test, expect, axios, GET } = cds.test(app)
+const { test, expect, axios, GET, POST, PATCH } = cds.test(app)
 axios.defaults.auth = { username: "alice" }
 const incidentID = "3ccf474c-3881-44b7-99fb-59a2a4668418"
 
 describe("Tests for uploading/deleting attachments through API calls - in-memory db", () => {
-  beforeAll(async () => {
-    // Set environment before any connections
-    cds.env.requires.db.kind = "sql"
-    cds.env.requires.attachments.kind = "db"
-    cds.env.requires.attachments.scan = false
-    cds.env.profiles = ["development"]
-
-    // Connect to services
-    await cds.connect.to("sql:my.db")
-    await cds.connect.to("attachments")
-  })
 
   beforeEach(async () => {
     await test.data.reset()
   })
 
-  //Draft mode uploading attachment
-  it("Requesting object page", async () => {
-
-    //read attachments list for Incident
-    const attachmentResponse = await GET(
-      `odata/v4/processor/Incidents(ID=${incidentID},IsActiveEntity=true)`
+  it("Localized values are stored", async () => {
+    await POST(
+      `odata/v4/processor/Incidents(ID=${incidentID},IsActiveEntity=true)/ProcessorService.draftEdit`, {}
     )
-    //the data should have only one attachment
-    expect(attachmentResponse.status).to.equal(200)
-    expect(attachmentResponse.data).to.not.be.undefined;
+
+    await PATCH(
+      `odata/v4/processor/Incidents(ID=${incidentID},IsActiveEntity=false)`, {
+        status_code: 'R'
+      }
+    )
+
+    await POST(
+      `odata/v4/processor/Incidents(ID=${incidentID},IsActiveEntity=false)/ProcessorService.draftActivate`, {}
+    )
+
+    const {data: {value: changes}} = await GET(
+      `odata/v4/processor/Incidents(ID=${incidentID},IsActiveEntity=true)/changes`
+    )
+    const statusChange = changes.find(change => change.attribute === 'status');
+    expect(statusChange).to.have.property('valueChangedFrom', 'New')
+    expect(statusChange).to.have.property('valueChangedTo', 'Resolved')
+
+    await POST(
+      `odata/v4/processor/Incidents(ID=${incidentID},IsActiveEntity=true)/ProcessorService.draftEdit`, {}
+    )
+
+    await PATCH(
+      `odata/v4/processor/Incidents(ID=${incidentID},IsActiveEntity=false)?sap-locale=de`, {
+        status_code: 'N'
+      }
+    )
+
+    await POST(
+      `odata/v4/processor/Incidents(ID=${incidentID},IsActiveEntity=false)/ProcessorService.draftActivate?sap-locale=de`, {}
+    )
+
+    const {data: {value: changes2}} = await GET(
+      `odata/v4/processor/Incidents(ID=${incidentID},IsActiveEntity=true)/changes`
+    )
+    const statusChangeGerman = changes2.sort((a,b) => b.createdAt - a.createdAt).find(change => change.attribute === 'status');
+    expect(statusChangeGerman).to.have.property('valueChangedFrom', 'Gelöst')
+    expect(statusChangeGerman).to.have.property('valueChangedTo', 'Neu')
   })
 })
