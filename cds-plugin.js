7d32fdf966a6f7ebbbbaf061cb60c1bf81a172bb
Stefan Rudi <stefan.rudi@sap.com>
2026-03-06 14:44:54 +0100

    refactor: move

--- cds-plugin.js
+++ cds-plugin.js
@@ -1,7 +1,7 @@
 const cds = require('@sap/cds');
 
 const { enhanceModel } = require('./lib/model-enhancer.js');
-const { registerSessionVariableHandlers } = require('./lib/trigger/deploy.js');
+const { registerSessionVariableHandlers } = require('./lib/skipHandlers.js');
 const { deploySQLiteTriggers } = require('./lib/sqlite/register.js');
 const { registerPostgresCompilerHook, deployPostgresLabels } = require('./lib/postgres/register.js');
 const { registerH2CompilerHook } = require('./lib/h2/register.js');
