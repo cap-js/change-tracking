b1499b8c218704d67e9938394bed7165bd27ed29
Stefan Rudi <stefan.rudi@sap.com>
2026-04-08 10:28:35 +0200

    chore: add eslint rule for .only in test files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,2 +1,16 @@
 import cds from '@sap/cds/eslint.config.mjs';
-export default [...cds];
+export default [
+	...cds,
+	{
+		files: ['tests/**/*.test.js'],
+		rules: {
+			'no-restricted-syntax': [
+				'error',
+				{
+					selector: "MemberExpression[property.name='only']",
+					message: '.only is not allowed in tests.'
+				}
+			]
+		}
+	}
+];
