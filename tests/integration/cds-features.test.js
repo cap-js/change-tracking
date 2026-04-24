69c7664bf6a14eae457cddc34b3f895b333548a1
Stefan Rudi <stefan.rudi@sap.com>
2026-04-24 16:02:07 +0200

    adjustments

--- tests/integration/cds-features.test.js
+++ tests/integration/cds-features.test.js
@@ -818,7 +818,7 @@ describe('CDS Features', () => {
 		});
 	});
 
-	it('statement-level trigger on SAP_BAI_CDH_DATASETS fails for CAP-generated JSON_TABLE inserts, but works for normal relational inserts.', async () => {
+	it.skip('statement-level trigger on SAP_BAI_CDH_DATASETS fails for CAP-generated JSON_TABLE inserts, but works for normal relational inserts.', async () => {
 		const { DataSets } = cds.entities('sap.dh');
 		// Arrange
 		const timeAtStart = Date.now()
