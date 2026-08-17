df30d86d7cb105644fde1426208967e7bad20baa
Stefan Rudi <stefan.rudi@sap.com>
2026-08-17 08:17:12 +0200

    test(vitest): adjust config

--- vitest.config.js
+++ vitest.config.js
@@ -1,19 +1,8 @@
-const isJavaEnv = /java/i.test(process.env.CDS_ENV || '');
-const javaOverrides = isJavaEnv
-  ? {
-      hookTimeout: 10 * 60_000,
-      teardownTimeout: 60_000,
-      maxWorkers: 1,
-      isolate: false,
-      fileParallelism: false
-    }
-  : {};
-
 export default {
   test: {
     silent: true,
     testTimeout: 120_000,
-    setupFiles: ['./tests/setup-java.js'],
-    ...javaOverrides
+    hookTimeout: 60_000,
+    setupFiles: ['./tests/setup-java.js']
   }
 };
